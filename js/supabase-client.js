b825615a86cbef53b25f91d1bd8427cb8777b972
happierinpanties <penultimatepanties@post.com>
2026-07-18 12:56:30 -0400

    Create supabase-client.js

---
+++ js/supabase-client.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+
+export const client = createClient(
+  'https://rkocfrsaeeyxgfdxeiqz.supabase.co',
+  'sb_publishable_yp7myP1S5wmYLQrgwNWxhg_IfZHdeZj'
+);
